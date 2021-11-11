efd39adab5fd48d0ab25d1620c7522ca900b4509
shkumar64 <57773810+shkumar64@users.noreply.github.com>
2021-11-11 13:33:48 -0800

    Delete Challenge1 directory

--- Challenge1/createNewView.sql
+++
@@ -1,7 +0,0 @@
-CREATE VIEW [dbo].[finalreport]
-	AS SELECT * FROM
-	 OPENROWSET( 
-       'CosmosDB',
-       'Account=<>yourcosmosaccoutname>;Database=fsi-marketdata;Key=<Youraccountkey>',
-       fintransactions) with (TransactionAmount BIGINT, isFraud INT, OFACviolation INT, State VARCHAR(200), TransactionType VARCHAR(200) ) as rows 
-
